574c633a4af0e6264c16dcc785f556c4006b6a27
Gerard <gerardpuig31@hotmail.com>
2021-04-19 19:41:42 +0200

    Create Player

--- src/main/java/com/itacademy/dicegame/controller/PlayerController.java
+++ src/main/java/com/itacademy/dicegame/controller/PlayerController.java
@@ -2,22 +2,36 @@ package com.itacademy.dicegame.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itacademy.dicegame.domain.Player;
+import com.itacademy.dicegame.service.PlayerService;
 
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
+	/*
+	 * Controller CRUD Players
+	 */
+	
+	@Autowired
+	PlayerService playerService;
 	
 	@PostMapping("")
-	public Player createplayer() {
-		return null;
+	@ResponseStatus(HttpStatus.CREATED)  // 201
+	public Player createplayer(@Valid @RequestBody Player player) {
+		return playerService.createPlayer(player);
 	}
 	
 	@PutMapping("")
--- src/main/java/com/itacademy/dicegame/service/PlayerService.java
+++ src/main/java/com/itacademy/dicegame/service/PlayerService.java
@@ -1,8 +1,26 @@
 package com.itacademy.dicegame.service;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.itacademy.dicegame.domain.Player;
+import com.itacademy.dicegame.persistence.PlayerRepository;
 
 @Service
 public class PlayerService {
+	
+	@Autowired
+	PlayerRepository playerRepository;
 
+	// createPlayer
+	public Player createPlayer(@Valid Player player) {
+		playerRepository.save(player);
+		return playerRepository.findById(player.getId()).get();
+	}
+
+	
+	
+	
 }
