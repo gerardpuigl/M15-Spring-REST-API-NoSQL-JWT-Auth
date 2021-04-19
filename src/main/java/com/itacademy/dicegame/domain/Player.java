901f47bf1500411f21b965d9a367a3d8c978945a
Gerard <gerardpuig31@hotmail.com>
2021-04-19 13:18:00 +0200

    Player Entity Creation

--- src/main/java/com/itacademy/dicegame/domain/Player.java
+++ src/main/java/com/itacademy/dicegame/domain/Player.java
@@ -1,5 +1,48 @@
 package com.itacademy.dicegame.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+
+@Entity
+@Table(name="Players")
 public class Player {
 
+	@Id
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="player_id")
+	private long id;
+	
+	@NotBlank
+	@Column(name="name")
+	private String name;
+	
+	public Player() {
+		this.name="ANÒNIM";
+	}
+	
+	public Player(String name) {
+		this.name=name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+		
 }
