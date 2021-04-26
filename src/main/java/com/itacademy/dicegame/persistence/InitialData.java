b01b62f4c28afc43ae1d42474ef07c5971a48db7
Gerard <gerardpuig31@hotmail.com>
2021-04-27 01:40:02 +0200

    Refactor Initial Data Creation

--- src/main/java/com/itacademy/dicegame/persistence/InitialData.java
+++ src/main/java/com/itacademy/dicegame/persistence/InitialData.java
@@ -17,39 +17,26 @@ public class InitialData {
 		@Autowired
 		private DiceGameRepository diceGameRepository;
 		
+		// populate the repository if it's empty
 	    @PostConstruct
 	    public void populateIfEmpty() {
 	    	if(playerRepository.findAll().size() == 0) {
-	    		Player player1 = new Player("TestPlayer1");
-	    		playerRepository.save(player1);
-	    		for (int i = 0; i < 10; i++) {
-	    			DiceGame game = new DiceGame(player1);
-	    			diceGameRepository.save(game);
-					player1.addGame(game);
-				}
-	    		player1.setWinPercentage();
-	    		playerRepository.save(player1);
-	    		
-	    		Player player2 = new Player("TestPlayer2");
-	    		playerRepository.save(player2);	    		
-	    		for (int i = 0; i < 7; i++) {
-	    			DiceGame game = new DiceGame(player2);
-	    			diceGameRepository.save(game);
-					player2.addGame(game);
-				}
-	    		player2.setWinPercentage();
-	    		playerRepository.save(player2);
-	    		
-	    		Player player3 = new Player("TestPlayer3");
-	    		playerRepository.save(player3);
-	    		for (int i = 0; i < 15; i++) {
-	    			DiceGame game = new DiceGame(player3);
-	    			diceGameRepository.save(game);
-					player3.addGame(game);
-				}
-	    		player3.setWinPercentage();
-	    		playerRepository.save(player3);
+	    		createTestPlayer("TestPlayer1",10);
+	    		createTestPlayer("TestPlayer2",7);
+	    		createTestPlayer("TestPlayer3",15);
 	    	};
 	    }
-	
+	    
+	    // create un player and "x" random games
+		private void createTestPlayer(String name, int initialGames) {
+			Player player = new Player(name);
+			playerRepository.save(player);
+			for (int i = 0; i < initialGames; i++) {
+				DiceGame game = new DiceGame(player);
+				diceGameRepository.save(game);
+				player.addGame(game);
+			}
+			player.setWinPercentage();
+			playerRepository.save(player);
+		}
 }
